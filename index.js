08eec24a241fe844f898364ce7c9e67eae29b279
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-11-10 09:09:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-function BoardMember(name, homeState, training) {
+// function BoardMember(name, homeState, training) {
+//   this.name = name;
+//   this.homeState = homeState;
+//   this.training = training;
+// }
+
+// BoardMember.prototype.veto = function() {
+//   return "No, I must disagree"
+// }
+
+
+// BoardMember.prototype.approve = function() {
+//   return "You can do that!"
+// }
+
+
+// BoardMember.prototype.doCharity = function() {
+//   return "I like to help people."
+// }
+
+
+// BoardMember.prototype.releasePressStatement = function() {
+//   return "You will see great things from Scuber."
+// }
+
+// BoardMember.prototype.sayHi = function() {
+//   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+// }
+
+function BoardMember (name, training, homeState) {
   this.name = name;
-  this.homeState = homeState;
   this.training = training;
+  this.homeState = homeState;
+}
+  
+Boardmember.prototype.veto() {
+    return "No, I must must disagree";
 }
 
-BoardMember.prototype.veto = function() {
-  return "No, I must disagree"
-}
-
-
-BoardMember.prototype.approve = function() {
+Boardmember.prototype.approve() {
   return "You can do that!"
 }
 
-
-BoardMember.prototype.doCharity = function() {
+BoardMember.prototype.doCharity() {
   return "I like to help people."
 }
 
-
-BoardMember.prototype.releasePressStatement = function() {
+BoardMember.prototype.releasePressStatement() {
   return "You will see great things from Scuber."
 }
 
-BoardMember.prototype.sayHi = function() {
+BoardMember.prototype.sayHi() {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
