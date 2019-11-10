8fded00ab885acb5d4f7849bc70d459c1e61f09c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-11-10 09:15:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ BoardMember.prototype.sayHi = function() {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 
-// function BoardMember (name, training, homeState) {
+// function BoardMember (name, homeState, training) {
 //   this.name = name;
 //   this.training = training;
 //   this.homeState = homeState;
