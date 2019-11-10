317e0bec8c89f794a35cfc91eb8523490156a3b8
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-11-10 09:15:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,36 +19,36 @@
 // }
 
 
-// BoardMember.prototype.releasePressStatement = function() {
-//   return "You will see great things from Scuber."
-// }
+BoardMember.prototype.releasePressStatement = function() {
+  return "You will see great things from Scuber."
+}
 
-// BoardMember.prototype.sayHi = function() {
-//   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-// }
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
 
-// function BoardMember (name, homeState, training) {
-//   this.name = name;
-//   this.training = training;
-//   this.homeState = homeState;
-// }
+function BoardMember (name, homeState, training) {
+  this.name = name;
+  this.training = training;
+  this.homeState = homeState;
+}
   
-// Boardmember.prototype.veto = function() {
-//     return "No, I must must disagree";
-// }
+Boardmember.prototype.veto = function() {
+    return "No, I must must disagree";
+}
 
-// Boardmember.prototype.approve = function() {
-//   return "You can do that!"
-// }
+Boardmember.prototype.approve = function() {
+  return "You can do that!"
+}
 
-// BoardMember.prototype.doCharity = function() {
-//   return "I like to help people."
-// }
+BoardMember.prototype.doCharity = function() {
+  return "I like to help people."
+}
 
-// BoardMember.prototype.releasePressStatement = function() {
-//   return "You will see great things from Scuber."
-// }
+BoardMember.prototype.releasePressStatement = function() {
+  return "You will see great things from Scuber."
+}
 
-// BoardMember.prototype.sayHi = function () {
-//   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-// }
+BoardMember.prototype.sayHi = function () {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
